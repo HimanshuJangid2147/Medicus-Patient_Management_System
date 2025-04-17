566e0551704ec4a3f5694acc108c388a21440b05
Himanshu jangid <83296281+HimanshuJangid2147@users.noreply.github.com>
2025-04-17 17:52:48 +0530

    Update medicalRecord.route.js

--- backend/routes/medicalRecord.route.js
+++ backend/routes/medicalRecord.route.js
@@ -14,7 +14,6 @@ const router = express.Router();
 router.post('/create', protectDoctorRoute, createMedicalRecord);
 router.get('/all', protectDoctorRoute, getAllMedicalRecords);
 router.get('/:id', protectRoute, getMedicalRecordById);
-// router.get('/patient', protectRoute, getMedicalRecordsByPatient);
 router.put('/:id', protectDoctorRoute, updateMedicalRecord);
 router.delete('/:id', protectDoctorRoute, deleteMedicalRecord);
 
